d3069b9e4c84e7b1a764d04c359bcc5cbb15f940
Sajibrasel <your-email@example.com>
2026-08-14 17:48:48 +0600

    Fix CSS path for server

--- movies/index.php
+++ movies/index.php
@@ -27,7 +27,7 @@ $showAds = shouldShowAds();
     <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🎬</text></svg>">
     
     <!-- CSS -->
-    <link rel="stylesheet" href="/movie_bot_new/movies/assets/css/style.css">
+    <link rel="stylesheet" href="/assets/css/style.css">
     
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
